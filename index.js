f543ece17c8835804ea1743d1d2f8a565dd1353e
snaphuman <fhernandezn@gmail.com>
2024-05-22 19:51:51 -0500

    initial commit with working version of persons restFull requests

---
+++ index.js
@@ -0,0 +1,74 @@
+const repl = require('node:repl');
+
+const express = require('express');
+const crypto = require('crypto');
+const app = express();
+app.use(express.json());
+
+const { persons } = require('./personsMock');
+
+app.get('/info', (req, res) => {
+    res.send('<h1>Hello World</h1>')
+});
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(person => person.id === id);
+
+    if(person) {
+        res.json(person);
+    } else {
+        res.statusMessage = 'Person not found';
+        res.status(400).end();
+    }
+});
+
+app.post('/api/persons', (req, res) => {
+
+    const body  = req.body;
+
+    if (!body.name) {
+        return res.status(400).json({
+            error: 'Name is missing',
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        uuid: generatedUUID(),
+    }
+
+    persons = persons.concat(person);
+
+    res.json(person);
+})
+
+app.delete('/api/persons/:uuid', (req, res) => {
+    const uuid = Number(req.params.uuid);
+    persons = persons.filter(person => person.uuid !== uuid)
+
+    res.status(204).end();
+});
+
+const generatedUUID = () => {
+    return crypto.randomUUID();
+}
+
+const PORT = 3001;
+
+app.listen(PORT);
+
+
+const replServer = repl.start({
+    prompt: 'my-app> ',
+    useColors: true,
+})
+
+replServer.context.app = app;
+
+console.log(`Server running on port ${PORT}`);
