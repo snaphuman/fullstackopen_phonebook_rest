f139025dfa617c431327d5bc926a6f3bb76012b3
snaphuman <fhernandezn@gmail.com>
2024-07-20 13:19:17 -0500

    apply initial automatic eslint fixes

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ app.use(express.static('dist'));
 morgan.token('content', (req, _res) => JSON.stringify(req.body));
 
 const logger = (tokens, req, res) => {
-    return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens['response-time'](req, res), 'ms',
-        tokens.content(req, res),
-    ]
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens['response-time'](req, res), 'ms',
+    tokens.content(req, res),
+  ]
 }
 
 app.use(morgan(logger));
@@ -37,113 +37,113 @@ const { error } = require('node:console');
 // Routes
 
 app.get('/info', (req, res) => {
-    const currentDate = new Date();
+  const currentDate = new Date();
 
-    Person.find({}).then(people => {
-        const total = people.length;
-        const html = `
+  Person.find({}).then(people => {
+    const total = people.length;
+    const html = `
             <p>Phonebook has info for ${total} people</p>
             <p>${currentDate}</p>
         `;
-        res.send(html);
-    })
+    res.send(html);
+  })
 });
 
 app.get('/api/persons', (req, res) => {
-    Person.find({}).then(people => {
-        res.json(people);
-    })
+  Person.find({}).then(people => {
+    res.json(people);
+  })
 });
 
 app.get('/api/persons/:uuid', (req, res, next) => {
-    Person.findById(req.params.uuid).then(person => {
-        if(person) {
-            res.json(person);
-        } else {
-            res.status(404).end();
-        }
-    }).catch(error => next(error))
+  Person.findById(req.params.uuid).then(person => {
+    if(person) {
+      res.json(person);
+    } else {
+      res.status(404).end();
+    }
+  }).catch(error => next(error))
 });
 
 app.put('/api/persons/:uuid', (req, res, next) => {
-    const { name, number } = req.body;
+  const { name, number } = req.body;
 
-    const person = {
-        name,
-        number,
+  const person = {
+    name,
+    number,
+  }
+
+  Person.findByIdAndUpdate(
+    req.params.uuid, 
+    person, 
+    {
+      new: true,
+      runValidators: true,
+      context: 'query'
     }
-
-    Person.findByIdAndUpdate(
-        req.params.uuid, 
-        person, 
-        {
-            new: true,
-            runValidators: true,
-            context: 'query'
-        }
-    )
-        .then((result) => {
-            res.json(result)
-        })
-        .catch((error) => next(error))
+  )
+    .then((result) => {
+      res.json(result)
+    })
+    .catch((error) => next(error))
 
 })
 
 app.post('/api/persons', (req, res, next) => {
-    const body  = req.body;
+  const body  = req.body;
 
-    // TODO get persons from db or apply mongodb validation for unique values;
-    const personExist = persons.find(person => person.name.toLowerCase() === body.name.toLowerCase())
+  // TODO get persons from db or apply mongodb validation for unique values;
+  const personExist = persons.find(person => person.name.toLowerCase() === body.name.toLowerCase())
 
-    if (!body.name) {
-        return res.status(400).json({
-            error: 'Name is missing',
-        }).end();
-    }
+  if (!body.name) {
+    return res.status(400).json({
+      error: 'Name is missing',
+    }).end();
+  }
 
-    if (personExist) {
-        return res.status(400).json({
-            error: 'Name must be unique',
-        }).end();
-    }
+  if (personExist) {
+    return res.status(400).json({
+      error: 'Name must be unique',
+    }).end();
+  }
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-    })
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
 
-    person.save().then(saved => {
-        res.json(saved);
-    })
+  person.save().then(saved => {
+    res.json(saved);
+  })
     .catch(error => next(error))
 })
 
 app.delete('/api/persons/:uuid', (req, res, next) => {
-    console.log('PARAMS', req.params);
-    Person.findByIdAndDelete(req.params.uuid)
-        .then(result => {
-            res.status(204).end();
-        })
-        .catch(error => next(error));
+  console.log('PARAMS', req.params);
+  Person.findByIdAndDelete(req.params.uuid)
+    .then(result => {
+      res.status(204).end();
+    })
+    .catch(error => next(error));
 });
 
 // Error Handler
 
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({error: 'Unknown Endpoint'})
+  res.status(404).send({error: 'Unknown Endpoint'})
 
 }
 
 const errorHandler = (error, req, res, next) => {
-    console.error(error);
+  console.error(error);
 
-    if (error.name === 'CastError') {
-        return res.status(400).send({error: 'Malformed ID'})
-    } else if(error.name === 'ValidationError') {
-        return res.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return res.status(400).send({error: 'Malformed ID'})
+  } else if(error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
 
-    next(error);
+  next(error);
 };
 
 app.use(unknownEndpoint);
@@ -152,12 +152,12 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 const replServer = repl.start({
-    prompt: 'my-app> ',
-    useColors: true,
+  prompt: 'my-app> ',
+  useColors: true,
 })
 
 replServer.context.app = app;
